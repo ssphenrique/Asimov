ef9f946ce34a89eecc883ee1d05310d12c6d3445
Pedro Henrique <pedro.henrique@acquamania.local>
2025-02-19 13:08:42 -0300

    Subida dos arquivos Asimov 19/02/2025, versão 1.0

--- PythonConceitosBasicos/SequenciasLoops/main.py
+++ PythonConceitosBasicos/SequenciasLoops/main.py
@@ -1,2 +1,7 @@
 lista_misturada = [0,1.1, "python", True, [1,2]]
-print(lista_misturada[-1][1])
+print(lista_misturada)
+print(len(lista_misturada))
+
+del lista_misturada[2]
+print(lista_misturada)
+print(len(lista_misturada))
